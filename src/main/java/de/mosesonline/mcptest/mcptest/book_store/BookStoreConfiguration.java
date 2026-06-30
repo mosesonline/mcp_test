fdf304f3d0054e015c286273bc5141a3a0118be4
Slawomir Messner <mosesonline@gmx.de>
2026-06-30 21:01:54 +0200

    updates

--- src/main/java/de/mosesonline/mcptest/mcptest/book_store/BookStoreConfiguration.java
+++ src/main/java/de/mosesonline/mcptest/mcptest/book_store/BookStoreConfiguration.java
@@ -6,7 +6,6 @@ import org.opensearch.client.opensearch.core.CountRequest;
 import org.opensearch.client.opensearch.core.CountResponse;
 import org.opensearch.client.opensearch.core.CreateRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 
